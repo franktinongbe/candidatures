edea817c132d03bef514d65d4a60ba0d4384ee13
franktinongbe <franktinongbe86@gmail.com>
2025-12-28 23:34:28 +0100

    updati

--- models/Candidat.js
+++ models/Candidat.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const CandidatSchema = new mongoose.Schema({
-  nom: { type: String, required: true },
+  nom: String,
   telephone: String,
   email: String,
   poste: String,
   cvPath: String,
   idPath: String,
-  createdAt: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now }
 });
 
-// Vérifie que tu exportes bien le modèle
+// Important : vérifie l'exportation
 module.exports = mongoose.models.Candidat || mongoose.model('Candidat', CandidatSchema);
